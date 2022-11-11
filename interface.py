d07f03b479b3405b9a7cc4c883918217a10a27cf
sagarssonawane <103529078+sagarssonawane@users.noreply.github.com>
2022-11-11 14:30:15 +0530

    This is wine_prediction

---
+++ interface.py
@@ -0,0 +1,27 @@
+from msilib.schema import Class
+from urllib import request
+from flask import FLASK,import jsonify,requests
+
+App=flask(__name__)
+
+
+@app.route('/wine_predict')
+def wine():
+    input_data=request.form
+    print("Input data",input_data)
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+flask(__name__)= FLASK(__main__):
